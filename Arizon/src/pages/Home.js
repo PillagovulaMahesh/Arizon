bf6055716dca6ed9abcdea8e8fc0591ac47bc7c0
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-04-30 21:43:14 +0530

    Update and rename Home.js.txt to Home.js

---
+++ Arizon/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import FeaturedProducts from "../components/FeaturedProducts";
+import Footer from "../components/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <FeaturedProducts />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
